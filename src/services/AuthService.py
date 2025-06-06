aa3dea662403537c0e8753e8f1d9655d2d5f8bcc
Arash Manouchehri <arashmanouchehri83@gmail.com>
2025-06-07 00:46:09 +0330

    update authentication service

--- src/services/AuthService.py
+++ src/services/AuthService.py
@@ -1,36 +1,56 @@
 from hashlib import sha256
-from repositories.UserRepository import get_user, add_user
+from repositories.UserRepository import UserRepository
 from models.user import User
 import re
 
-class AuthServce:
+class AuthService:
 
     def __init__(self):
-        pass
+        self.user_repo = UserRepository()
 
-    def _validate(self, user_name, passwd, phone_number):
+    def validate_username(self, user_name):
         if not re.fullmatch(r'[A-Za-z0-9_.]{3:}', user_name):
-            raise Exception("Invalid Username")
-        if not re.fullmatch(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", passwd):
-            raise Exception('Invalid Psssword')
-        if len(phone_number) != 11:
-            raise Exception('Invalid Phone number')
+            return False
         return True
     
+    def validate_password(self, passwd):
+        if not re.fullmatch(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", passwd):
+            return False, None
+        return True, sha256(passwd)
+
+    def validate_phone_number(self, phone_number):
+        if len(phone_number) != 11:
+            return False
+        return True
+    
+    def get_hash(self, passwd):    
+        return sha256(passwd)
+    
+    def _validate(self, user_name, passwd, phone_number):
+        if not re.fullmatch(r'[A-Za-z0-9_.]{3:}', user_name):
+            return False, "Invalid Username"
+        if not re.fullmatch(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", passwd):
+            return False, "Invalid Psssword"
+        if len(phone_number) != 11:
+            return False, "Invalid Phone number"
+        return True, None
+    
     def register(self, user_name, passwd, phone_number):
-        if self._validate(user_name, passwd, phone_number):
-            if get_user(user_name):
-                raise Exception("Username Already Exists")
+        validate, err = self._validate(user_name, passwd, phone_number)
+        if validate:
+            if self.user_repo.get_user(user_name):
+                return False, "Username Already Exists"
             
-            add_user(User(user_name, sha256(passwd), phone_number))
-            return True
+            self.user_repo.add_user(User(user_name, sha256(passwd), phone_number))
+            return True, None
+        return False, err
         
     def sign_in(self, user_name, passwd):
         try:
-            user = get_user(user_name)
+            user = self.user_repo.get_user(user_name)
             if user.passwd_hash == sha256(passwd):
                 return user
         except:
-            raise Exception('Incorrect Username or Password')
+            return False
         
-        
+    
