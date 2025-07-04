efcd3e2fa8278f6e9654652f96a334d3e3860e83
Arash Manouchehri <arashmanouchehri83@gmail.com>
2025-07-04 17:24:02 +0330

    update the update method in userservice

--- src/services/UserService.py
+++ src/services/UserService.py
@@ -15,17 +15,35 @@ class UserService:
         return self.auth_service.register(user_name, passwd, phone_number)
     
     def update(self, user: User, new_username=None, new_passwd=None, new_phone=None, new_pic = None):
-        new_username = new_username if self.auth_service.validate_username(new_username) else user.username
-        new_passwd_hash = self.auth_service.get_hash(new_passwd) if self.auth_service.validate_password(new_passwd) else user.passwd_hash
-        new_phone = self.auth_service.validate_phone_number(new_phone) if new_phone else user.phone_number
-        new_pic = new_pic if new_pic else user.profile_pic
+        try:
+            if new_username and not self.auth_service.validate_username(new_username):
+                return False, 'invalid username format'
+            if new_passwd and not self.auth_service.validate_password(new_passwd):
+                return False, 'invalid password format'
+            if new_phone and not self.auth_service.validate_phone_number(new_phone):
+                return False, 'invalid phone number format'
 
-        user.username = new_username
-        user.passwd_hash = new_passwd_hash
-        user.phone_number = new_phone
-        user.profile_pic = new_pic
+            updated_username = new_username if new_username else user.username
+            updated_passwd_hash = self.auth_service.get_hash(new_passwd) if new_passwd else user.passwd_hash
+            updated_phone = new_phone if new_phone else user.phone_number
+            updated_pic = new_pic if new_pic else user.profile_pic
 
-        self.user_repo.update_user(user)
+            # Check for username and phone uniqueness
+            if updated_username != user.user_name and self.user_repo.get_user(updated_username):
+                return False, 'username already exists'
+            if updated_phone != user.phone_number and self.user_repo.get_user_by_phone(updated_phone):
+                return False, 'phone number already exists'
+
+            # Update user fields
+            user.user_name = updated_username
+            user.passwd_hash = updated_passwd_hash
+            user.phone_number = updated_phone
+            user.profile_pic = updated_pic
+
+            self.user_repo.update_user(user)
+            return True, 'user info updated succesfully'
+        except Exception as e:
+            return False, f'an error happend while updating user info : {e}'
 
     def find_user(self, user_name: str) -> User | None:
         try:
